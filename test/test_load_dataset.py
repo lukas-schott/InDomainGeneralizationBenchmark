cba40ad5ad67a9db57562d533ae42ae6354c5d81
Chris Russell <cmruss@amazon.com>
2021-05-20 13:16:52 +0200

    licence

--- test/test_load_dataset.py
+++ test/test_load_dataset.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 import numpy as np
 
 import lablet_generalization_benchmark.load_dataset as load_dataset
